037150a1f9cb52b443b33ff76597fe77740315ba
Chandan010895 <149336376+Chandan010895@users.noreply.github.com>
2023-11-13 23:29:46 +0100

    Add files via upload
    Adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    description="Math Problems and Points",
+    author="Chandan010895",
+    author_email="chandanupadhyay0994@gmail.com",
+    url="https://github.com/Chandan010895/dsss_homework_2.git",
+    packages=["math_quiz"],
+    install_requires=[],
+)
