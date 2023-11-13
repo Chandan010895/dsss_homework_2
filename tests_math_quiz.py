de17abda6e339bf33e1fe76cd461e506c5bc25c0
Chandan010895 <149336376+Chandan010895@users.noreply.github.com>
2023-11-13 23:10:10 +0100

    Update tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,24 +1,25 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import function_a, function_b, function_c
 
 
-class TestMathGame(unittest.TestCase):
+class TestMathQuizFunctions(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_function_a(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = function_a(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_function_b(self):
         # Test if the result is one of the specified operators
         for _ in range(1000):  # Test a large number of random values
-            rand_operator = function_B()
+            rand_operator = function_b()
             self.assertIn(rand_operator, ['+', '-', '*'])
 
-    def test_function_C(self):
+    def test_function_c(self):
+        # Test the calculation of the arithmetic operation for different cases
         test_cases = [
             (5, 2, '+', '5 + 2', 7),
             (8, 3, '-', '8 - 3', 5),
@@ -27,7 +28,7 @@ class TestMathGame(unittest.TestCase):
         ]
 
         for num1, num2, operator, expected_problem, expected_answer in test_cases:
-            result = function_C(num1, num2, operator)
+            result = function_c(num1, num2, operator)
             expected_result = (expected_problem, expected_answer)
             self.assertEqual(result, expected_result)
 
